ee76a69ad716bd508d66ebccbe0bd504fe895cb4
Meghna2004p <meghnamohanan2004@gmail.com>
2024-10-30 10:58:21 +0530

    Add files via upload

---
+++ oct_week3/prg19_list of same,different&common.py
@@ -0,0 +1,19 @@
+list1=(input("enter the first list separated by spaces:")).split()
+list2=(input("enter the second list separated by spaces:")).split()
+for i in range(len(list1)):
+    list1[i]=int(list1[i])
+for i in range(len(list2)):
+    list2[i]=int(list2[i])
+if(len(list1))==(len(list2)):
+    print("the list are of the same length")
+else:
+    print("the list are of the different length")
+if(sum(list1)==sum(list2)):
+    print("list sum to same value")
+else:
+    print("list sum to different value")
+common_value=set(list1)&set(list2)
+if common_value:
+    print("common_value",common_value)
+else:
+    print("no common_value")
